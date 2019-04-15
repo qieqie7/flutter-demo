ed5b2721f2853ea41bf960642847505665100752
exciting <exciting@MacBook-Pro.local>
2019-04-15 11:36:10 +0800

    stateful

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class TutorialHome extends StatelessWidget {
       ),
       //body占屏幕的大部分
       body: new Center(
-        child: new MyButton(),
+        child: new Counter(),
       ),
       floatingActionButton: new FloatingActionButton(
         tooltip: 'Add', // used by assistive technologies
@@ -43,25 +43,46 @@ class TutorialHome extends StatelessWidget {
   }
 }
 
-class MyButton extends StatelessWidget {
+class Counter extends StatefulWidget {
+  // This class is the configuration for the state. It holds the
+  // values (in this nothing) provided by the parent and used by the build
+  // method of the State. Fields in a Widget subclass are always marked "final".
+
+  @override
+  _CounterState createState() => new _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      // This call to setState tells the Flutter framework that
+      // something has changed in this State, which causes it to rerun
+      // the build method below so that the display can reflect the
+      // updated values. If we changed _counter without calling
+      // setState(), then the build method would not be called again,
+      // and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new GestureDetector(
-      onTap: () {
-        print('MyButton was tapped!');
-      },
-      child: new Container(
-        height: 36.0,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: new BoxDecoration(
-          borderRadius: new BorderRadius.circular(5.0),
-          color: Colors.lightGreen[500],
+    // This method is rerun every time setState is called, for instance
+    // as done by the _increment method above.
+    // The Flutter framework has been optimized to make rerunning
+    // build methods fast, so that you can just rebuild anything that
+    // needs updating rather than having to individually change
+    // instances of widgets.
+    return new Row(
+      children: <Widget>[
+        new RaisedButton(
+          onPressed: _increment,
+          child: new Text('Increment'),
         ),
-        child: new Center(
-          child: new Text('Engage'),
-        ),
-      ),
+        new Text('Count: $_counter'),
+      ],
     );
   }
 }
