2a27cbec639594cc1263b49304970a84fb6e328b
exciting <exciting@MacBook-Pro.local>
2019-04-15 11:23:27 +0800

    使用 Material 组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'My App',
-      home: new MyScaffold(),
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    title: 'Flutter Tutorial',
+    home: new TutorialHome(),
+  ));
 }
 
-class MyScaffold extends StatelessWidget {
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Material(
-      child: new Column(
-        children: <Widget>[
-          new MyAppBar(
-            title: new Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.title,
-            )
-          ),
-          new Expanded(
-            child: new Center(
-              child: new Text('Hello world'),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-
-  // Widget子类中的字段往往都会定义为"final"
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      height: 56.0, // 单位是逻辑上的像素（并非真实的像素，类似于浏览器中的像素）
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: new BoxDecoration(color: Colors.blue[500]),
-      // Row 是水平方向的线性布局（linear layout）
-      child: new Row(
-        //列表项的类型是 <Widget>
-        children: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null 会禁用 button
-          ),
-          // Expanded expands its child to fill the available space.
-          new Expanded(
-            child: title,
-          ),
+    //Scaffold是Material中主要的布局组件.
+    return new Scaffold(
+      appBar: new AppBar(
+        // 左侧 Icon
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        // 中间 title
+        title: new Text('Example title'),
+        // 右侧 action
+        actions: <Widget>[
           new IconButton(
             icon: new Icon(Icons.search),
             tooltip: 'Search',
             onPressed: null,
           ),
         ],
+      ),
+      //body占屏幕的大部分
+      body: new Center(
+        child: new Text('Hello, world!'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: new Icon(Icons.add),
+        onPressed: null,
       ),
     );
   }
