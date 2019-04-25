cd2c93f45fe21933c3c445b76e91e3ce42623ff0
exciting <514642267@qq.com>
2019-04-25 20:00:02 +0800

    add Router & Basic_widget.dart modified into sub-routing

--- lib/basic_widget.dart
+++ lib/basic_widget.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class BasicWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new CupertinoApp(
-      title: 'basic_widget',
-      home: CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text("Cupertino Demo"),
-        ),
-        child: Center(
-          child: CupertinoButton(
-              color: CupertinoColors.activeBlue,
-              child: Text("Press"),
-              onPressed: () {}),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Basic Widget"),
+      ),
+      body: Center(
+        child: FlatButton(
+            color: Colors.orange,
+            child: Text("Press"),
+            onPressed: () {
+              print('ops! pressed');
+            }
         ),
       )
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'basic_widget.dart';
+
 // import 'life_cycle.dart';
 // import 'state.dart';
-// import 'basic_widget.dart';
 // import 'text.dart';
 // import 'button.dart';
 // import 'image.dart';
@@ -18,13 +19,39 @@ import 'package:flutter/widgets.dart';
 // import 'transform.dart';
 // import 'container.dart';
 // import 'single_child_scroll_view.dart';
-import 'list_view.dart';
+// import 'list_view.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+    return MaterialApp(
+      title: 'Flutter 实战',
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter 实战'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('基础Widget', style: TextStyle(color: Colors.blue)),
+            onTap: () {
+              Navigator.of(context).push(
+                new MaterialPageRoute(builder: (context) => BasicWidget())
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
